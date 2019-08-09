5b1748e0b3a1d84b6a291c84a94bc8878908e263
Shahrani Bin Rahiman <shahranibinrahiman@gmail.com>
2019-08-09 15:31:27 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+//four variables, queen or king
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//User flipped over two cards
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
