53aad963604393ec93102763522d2e6652bd2652
takerbiker <shahranibinrahiman@gmail.com>
2019-08-21 12:43:54 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,7 @@
 //array to store four cards
-const cards = ["queen","queen","king","king"];
-cardsInPlay = [];
+const cardOne = "queen";
+const cardTwo = "queen";
+const cardThree = "king";
+const cardFour = "king";
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped queen");
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You have found a match!');
-  } else if (cardsInPlay[0] != cardsInPlay[1]) {
-    alert('Sorry, try again');
-  }
-}
-
-
-
-
-function flipCard(cardId) {
-
-}
+console.log("User flipped " + cardTwo);
