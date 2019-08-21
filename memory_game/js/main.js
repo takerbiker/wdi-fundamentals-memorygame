f13f23c1c839d94f3675c4a459fb833d30d088bd
takerbiker <shahranibinrahiman@gmail.com>
2019-08-21 19:05:03 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+},
+];
 const cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,13 +35,17 @@ function checkForMatch() {
 
 function flipcard(cardId) {
     checkForMatch();
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 };
 
 
 flipcard(0);
 flipcard(2);
+
+
 
 
 //stopped at functions unit 9
